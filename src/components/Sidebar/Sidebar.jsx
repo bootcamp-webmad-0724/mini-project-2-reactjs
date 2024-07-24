0bf83d2c7097e9f43ddc90e02e4a4ec075e41029
Ironhack <ironhack@Ironhacks-MacBook-Pro.local>
2024-07-24 14:33:24 +0200

    day 1 solved

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className="Sidebar">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Apartamentos</a>
+                    </li>
+                    <li>
+                        <a href="#">Otros</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Sidebar
